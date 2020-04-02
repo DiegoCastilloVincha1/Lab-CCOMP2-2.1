06fce424f36e4039fafcf9478f6dbd1daa0ab7c9
DiegoCastilloVincha1 <diego.castillo@ucsp.edu.pe>
2020-04-02 11:53:51 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello World\n";
+	cout << "Ciencia de la computacion\n";
+	int x = 5, y = 2;
+	int z = x + y;
+	cout << "la suma de x+y es " << z << endl;
+	int a;
+	cout << "ingresa un numero\t";  cin >> a;
+	cout << "el valor de a es " << a << "\n";
+	
+	return 0;
+}
