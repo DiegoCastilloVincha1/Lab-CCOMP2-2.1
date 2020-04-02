7ad7b8530acbaed82d769b72de97acd8c331f703
DiegoCastilloVincha1 <diego.castillo@ucsp.edu.pe>
2020-04-02 12:21:07 -0500

    modificar main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,7 @@ int main()
 {
 	cout << "Hello World\n";
 	cout << "Ciencia de la computacion\n";
-	int x = 5, y = 2;
-	int z = x + y;
-	cout << "la suma de x+y es " << z << endl;
-	int a;
-	cout << "ingresa un numero\t";  cin >> a;
-	cout << "el valor de a es " << a << "\n";
+	
 	
 	return 0;
 }
